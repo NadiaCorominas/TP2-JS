57a24716f3bb163c23d475eb74521e06ece35d0d
Nadia Corominas <nadiacorominas@gmail.com>
2023-10-01 16:57:55 -0300

    estrellas

--- js/detalles.js
+++ js/detalles.js
@@ -1,6 +1,23 @@
 const destacado = JSON.parse(localStorage.getItem("producto"));
 
 const detalleProducto = document.querySelector("#producto");
+
+function crearEstrellas(puntuacion) {
+  const estrellasContainer = document.createElement('div');
+  estrellasContainer.className = 'estrellas';
+
+  for (let i = 1; i <= 5; i++) {
+    const estrella = document.createElement('i');
+    if (i <= puntuacion) {
+      estrella.className = 'fas fa-star'; 
+    } else {
+      estrella.className = 'far fa-star';
+    }
+    estrellasContainer.appendChild(estrella);
+  }
+
+  return estrellasContainer;
+}
  
 destacado.forEach((product) => {
     let content = document.createElement("div");
@@ -16,15 +33,17 @@ destacado.forEach((product) => {
     <p class="precios">$ ${product.precio}</p>
 </div>
 <div>
-    <p class="puntuacion">$ ${product.puntuacion}</p>
-</div>
-<div>
 
     `;
-
+    const puntuacion = product.puntuacion; // Valor numérico de puntuación
+    const estrellas = crearEstrellas(puntuacion);
+  
+    content.appendChild(estrellas);
     detalleProducto.append(content);
 });
 
+
+  
 localStorage.removeItem(info);
 
 
