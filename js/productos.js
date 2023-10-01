589f6dc837579d117dbbf98266c65331e2c58692
Nadia Corominas <nadiacorominas@gmail.com>
2023-10-01 16:23:59 -0300

    Tratando de solucionar error en pages

--- js/productos.js
+++ js/productos.js
@@ -27,3 +27,8 @@ destacado.forEach((product) => {
 
 localStorage.removeItem(info);
 
+
+
+
+  
+  
--- js/script.js
+++ js/script.js
@@ -1,112 +1,97 @@
 
-let product;
+// let product;
 
- const productos = async () => {
+//  const productos = async () => {
 
-  //localStorage.clear();
+//   //localStorage.clear();
   
 
-product = localStorage.getItem("product");
+// product = localStorage.getItem("product");
 
-if (product == null) {
+// if (product == null) {
 
-const response = await fetch ("productos.json");
-product = await response.json();
+// const response = await fetch ("productos.json");
+// product = await response.json();
 
-localStorage.setItem("product", JSON.stringify(product));
+// localStorage.setItem("product", JSON.stringify(product));
 
-}
-if (typeof product == "string") {
-  product = JSON.parse(product);
-};
+// }
+// if (typeof product == "string") {
+//   product = JSON.parse(product);
+// };
 
-};
+// };
 
-productos();
+// productos();
 
-// const jsonProductos = JSON.parse(localStorage.getItem("productos"));
+//   const jsonProductos = JSON.parse(localStorage.getItem("productos"));
+
+// const tiendaProductos = document.querySelector("#listado");
+
+// const info = [];
 
 // if (jsonProductos !== null) {
-//   // Ahora puedes iterar sobre jsonProductos de manera segura
 //   jsonProductos.forEach((product) => {
-//     // ...
+//     let content = document.createElement("div");
+//     content.innerHTML = `
+//       <h3>${product.nombre}</h3>
+//       <img class="imagen" src="${product.image}">
+//     `;
+
+//     tiendaProductos.append(content);
+
+//     let detalle = document.createElement("button");
+//     detalle.innerText = "+ Info";
+//     content.append(detalle);
+//     detalle.className = "info";
+
+//     detalle.addEventListener("click", () => {
+//       info.push({
+//         id: product.id,
+//         nombre: product.nombre,
+//         imagen: product.image,
+//         descripcion: product.descripcion,
+//         precio: product.precio,
+//         puntuacion: product.puntuacion,
+//       });
+
+//       localStorage.setItem("producto", JSON.stringify(info));
+
+//       window.location = "detalles.html";
+//     });
 //   });
 // }
 
-  // const jsonProductos = JSON.parse(localStorage.getItem("productos"));
+document.addEventListener("DOMContentLoaded", async () => {
+  const response = await fetch('productos.json');
+  const productos = await response.json();
+  const listadoContainer = document.getElementById('listado');
 
-  const jsonProductos = JSON.parse(localStorage.getItem("productos"));
+  productos.forEach(producto => {
+    const productoDiv = document.createElement('div');
+    productoDiv.classList.add('producto');
 
-const tiendaProductos = document.querySelector("#listado");
+    const imagen = document.createElement('img');
+    imagen.src = producto.image;
+    imagen.alt = producto.nombre;
+    imagen.className = "imagen";
 
-const info = [];
+    const nombre = document.createElement('h3');
+    nombre.textContent = producto.nombre;
 
-if (jsonProductos !== null) {
-  jsonProductos.forEach((product) => {
-    let content = document.createElement("div");
-    content.innerHTML = `
-      <h3>${product.nombre}</h3>
-      <img class="imagen" src="${product.image}">
-    `;
+    const boton = document.createElement('button');
+    boton.textContent = '+ Info';
+    boton.className = "info";
 
-    tiendaProductos.append(content);
-
-    let detalle = document.createElement("button");
-    detalle.innerText = "+ Info";
-    content.append(detalle);
-    detalle.className = "info";
-
-    detalle.addEventListener("click", () => {
-      info.push({
-        id: product.id,
-        nombre: product.nombre,
-        imagen: product.image,
-        descripcion: product.descripcion,
-        precio: product.precio,
-        puntuacion: product.puntuacion,
-      });
-
-      localStorage.setItem("producto", JSON.stringify(info));
-
-      window.location = "detalles.html";
+        boton.addEventListener('click', () => {
+      localStorage.setItem('productoSeleccionado', JSON.stringify(producto));
+      window.location.href = 'detalles.html';
     });
+
+    productoDiv.appendChild(imagen);
+    productoDiv.appendChild(nombre);
+    productoDiv.appendChild(boton);
+
+    listadoContainer.appendChild(productoDiv);
   });
-}
-
-  // const tiendaProductos = document.querySelector("#listado");
-
-  // const info = [];
-
-  // jsonProductos.forEach((product) => {
-  //   let content = document.createElement("div");
-  //      content.innerHTML = `
-  //       <h3>${product.nombre}</h3>
-  //       <img class="imagen" src="${product.image}">
-  //   `;
-
-  //   tiendaProductos.append(content);
-
-  //   let detalle = document.createElement("button");
-  //   detalle.innerText = "+ Info";
-  //   content.append(detalle);
-  //   detalle.className = "info";
-
-  //   detalle.addEventListener("click", () => {
-  //     info.push({
-  //       id: product.id,
-  //       nombre: product.nombre,
-  //       imagen: product.image,
-  //       descripcion: product.descripcion,
-  //         precio: product.precio,
-  //         puntuacion: product.puntuacion,
-  //     });
-
-  //     localStorage.setItem("producto", JSON.stringify(info));
-
-  //     window.location ="detalles.html";
-
-  //   });
-
-  // });
-
-  
+});
